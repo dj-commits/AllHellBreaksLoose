69c1358887bb9e7d97bcab6c97070dca319e9109
dj-commits <djcommits@gmail.com>
2022-11-06 11:26:01 -0600

    Final push 11/6/22

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -63,6 +63,7 @@ public class PlayerController : MonoBehaviour
             {
 
                 CreateBullet();
+                canShoot = false;
                 StartCoroutine(shootWaitTimer());
                 
             }
