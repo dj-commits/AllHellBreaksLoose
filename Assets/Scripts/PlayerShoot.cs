a3ae1b9c2350d9d66cce4580df8aa5df30144e62
Matthew <mschrum89@gmail.com>
2022-11-06 09:02:39 -0600

    Added logic for shooting

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject bullet;
+    public float bulletSpeed = 5.0f;
+
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButton("Shoot"))
+        {
+            Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y + 1);
+            Vector2 direction = target - myPos;
+            direction.Normalize();
+
+            float bulletAngle = Vector2.SignedAngle(Vector2.up, direction);
+            Quaternion rotation = Quaternion.Euler(new Vector3(0, 0, bulletAngle));
+
+
+            GameObject projectile = (GameObject)Instantiate(bullet, myPos, rotation);
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
+        }
     }
 }
