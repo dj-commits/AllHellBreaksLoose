c8dff51e78bf4be85482a8d395d9aa0b87d8de56
dj-commits <djcommits@gmail.com>
2022-11-29 17:07:56 -0600

    Pushing priestEnemy prefab changes

--- Assets/Scripts/Enemies/PriestEnemy.cs
+++ Assets/Scripts/Enemies/PriestEnemy.cs
@@ -13,6 +13,8 @@ public class PriestEnemy : Enemy
     [SerializeField]
     float timeUntilNextSummon;
     [SerializeField]
+    float timeUntilSummonAnimFinished;
+    [SerializeField]
     bool canSummon;
 
     // Start is called before the first frame update
@@ -35,16 +37,21 @@ public class PriestEnemy : Enemy
 
     public void Summon(GameObject summon)
     {
-        float randomSummonFloat = Random.Range(-1, summonRange);
-        Vector3 summonSpawnPos = this.transform.position * randomSummonFloat;
+        float randomXSummonFloat = Random.Range(-1, summonRange);
+        float randomYSummonFloat = Random.Range(-1, summonRange);
+        Vector3 summonSpawnPos = new Vector3(transform.position.x + randomXSummonFloat, transform.position.y + randomYSummonFloat, transform.position.z);
         animator.SetBool("IsSummoning", true);
         GameObject summonClone = Instantiate(summon, summonSpawnPos, Quaternion.identity);
         StartCoroutine(SummonIntervalTimer());
     }
-
+    IEnumerator SummonAnimTimer()
+    {
+        yield return new WaitForSeconds(timeUntilSummonAnimFinished);
+    }
     IEnumerator SummonIntervalTimer()
     {
         yield return new WaitForSeconds(timeUntilNextSummon);
+        animator.SetBool("IsSummoning", false);
         canSummon = true;
         
     }
