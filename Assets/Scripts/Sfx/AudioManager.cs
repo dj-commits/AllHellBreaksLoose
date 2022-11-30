a47a8f62fe3205f2b7a1006e05c42c1339f000e8
Matthew <mschrum89@gmail.com>
2022-11-29 21:01:46 -0600

    Set default volume on sound creation back to default value.

--- Assets/Scripts/Sfx/AudioManager.cs
+++ Assets/Scripts/Sfx/AudioManager.cs
@@ -32,7 +32,7 @@ public class AudioManager : MonoBehaviour
         {
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
-            s.source.volume = globalVolume;
+            s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
